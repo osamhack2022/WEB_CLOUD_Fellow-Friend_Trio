410f524e769a21791285d54428795520968b50ba
Gwemin <ironjustlikethat@gmail.com>
2022-09-23 03:38:00 +0000

    routes settings

--- WEB(BE)/index.js
+++ WEB(BE)/index.js
@@ -1,8 +1,13 @@
 const app = require('express')()
 
+const usersRouter = require('./routes/users')
+
 app.get('/', (req,res) => {
-    res.send("Start🙌")
+    res.send("Default Pages")
 })
+
+app.use("/users", usersRouter)
+app.use("/chat", usersRouter);
 
 app.listen(3000, () => {
     console.log("서버가 작동중입니다~🎶")
