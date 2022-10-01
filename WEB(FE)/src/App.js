3a0592684cb4df630d193c5afb5aedb8f54f086d
komi22 <92563389+komi22@users.noreply.github.com>
2022-10-01 02:51:49 +0000

    template push

--- WEB(FE)/src/App.js
+++ WEB(FE)/src/App.js
@@ -1,10 +1,25 @@
+import React, { component } from 'react';
+import SignUp from '/modules/Signup';
 
+
+class App extends component {
+  render() {
+    return (
+      <div className="App">
+        <SignUp>
+        </SignUp>
+      </div>
+    );
+  }
+}
+
+/*
 function App() {
   return (
     <div className="App">
       test
     </div>
   );
-}
+} */
 
 export default App;
