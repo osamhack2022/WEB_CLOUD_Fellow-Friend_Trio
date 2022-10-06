97e34e081d2b3ec958734ff93a23d949fd090329
Gwemin <ironjustlikethat@gmail.com>
2022-10-06 10:44:34 +0000

    template /modules/* remove

--- WEB(FE)/src/App.js
+++ WEB(FE)/src/App.js
@@ -1,24 +1,22 @@
-import React, { component } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { component } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import SignUp from '/workspaces/WEB_Fellow-Friend_Trio/WEB(FE)/src/modules/SignUp.js';
-import SignIn from '/workspaces/WEB_Fellow-Friend_Trio/WEB(FE)/src/modules/SignIn.js';
-import Index from '/workspaces/WEB_Fellow-Friend_Trio/WEB(FE)/src/modules/Home.js';
-import ForgotPassword from './modules/ForgotPassword';
-
+import SignUp from '/workspaces/WEB_Fellow-Friend_Trio/WEB(FE)/src/modules/SignUp.js'
+import SignIn from '/workspaces/WEB_Fellow-Friend_Trio/WEB(FE)/src/modules/SignIn.js'
+import Index from '/workspaces/WEB_Fellow-Friend_Trio/WEB(FE)/src/modules/Home.js'
+import ForgotPassword from './modules/ForgotPassword'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Index />}></Route>
-        <Route path="/login" element={<SignIn />}></Route>
-        <Route path="/register" element={<SignUp />}></Route>
-        <Route path="/f_pw" element={<ForgotPassword />}></Route>
+        <Route path="/" element={<Index />} />
+        <Route path="/login" element={<SignIn />} />
+        <Route path="/register" element={<SignUp />} />
+        <Route path="/f_pw" element={<ForgotPassword />} />
       </Routes>
     </BrowserRouter>
-
-  );
+  )
 }
 
-export default App;
+export default App
