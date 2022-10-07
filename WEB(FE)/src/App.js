fc976af2c90e5f6fd8934bb9dc3c3ff39be4c0fb
Gwemin <ironjustlikethat@gmail.com>
2022-10-07 11:01:25 +0000

    routes 설정

--- WEB(FE)/src/App.js
+++ WEB(FE)/src/App.js
@@ -1,18 +1,16 @@
-import React, { component } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
-import SignUp from '/workspaces/WEB_Fellow-Friend_Trio/WEB(FE)/src/modules/SignUp.js'
-import SignIn from '/workspaces/WEB_Fellow-Friend_Trio/WEB(FE)/src/modules/SignIn.js'
-import Index from '/workspaces/WEB_Fellow-Friend_Trio/WEB(FE)/src/modules/Home.js'
-import ForgotPassword from './modules/ForgotPassword'
+import ForgotPassword from './components/views/ForgotPassword'
+import Home from './components/views/Home'
+import SignIn from './components/views/SignIn'
+import Signup from './components/views/Signup'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Index />} />
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<SignIn />} />
-        <Route path="/register" element={<SignUp />} />
+        <Route path="/register" element={<Signup />} />
         <Route path="/f_pw" element={<ForgotPassword />} />
       </Routes>
     </BrowserRouter>
---
+++ WEB(FE)/src/components/views/Home.jsx
@@ -0,0 +1,14 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <Typography textColor="neutral.500" fontSize="sm" fontWeight="lg">
+        Hello world!
+      </Typography>
+    </div>
+  )
+}
+
+export default Home
